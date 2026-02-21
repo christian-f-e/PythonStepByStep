7184b60c0f9ae94897526fcee417e2aad1d81620
Christian.Fernandez <chrfereli@hotmail.com>
2026-02-20 21:17:45 -0600

    SecuenciasDeEscape.py

---
+++ Seccion2/SecuenciasDeEscape.py
@@ -0,0 +1,7 @@
+"""Secuencias de escape"""
+# comentarios ignorados por el compilador
+curso = "Ultimate \"Python\""
+print(curso)
+
+saltoDeLinea = "vamos a \nseparar \nlas palabras"
+print(saltoDeLinea)
