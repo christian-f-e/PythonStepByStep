e3a5176c49602a251013699c437d2e3e3002f0a2
Christian.Fernandez <chrfereli@hotmail.com>
2026-02-20 20:54:34 -0600

    MetodosDeStrings.py

---
+++ Seccion2/MetodosDeStrings.py
@@ -0,0 +1,18 @@
+
+"""Metodos de Strings"""
+
+animal = " chanchito feliz "
+
+print(animal)
+print(animal.upper())
+print(animal.lower())
+print(animal.capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.strip().capitalize())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("CH"))
+print(animal.replace("nch", "j"))
+print("nch" in animal)
+print("nch" not in animal)
