fb131ede3702acaa8b90df85af672c7c0a72aebf
Christian.Fernandez <chrfereli@hotmail.com>
2026-02-20 21:36:43 -0600

    Numeros.py

---
+++ Seccion2/numeros.py
@@ -0,0 +1,18 @@
+"""Numeros.py"""
+
+numero = 2  # integer
+decimal = 1.2  # float
+imaginario = 2 + 2j  # imaginario
+# numero = numero + 2
+numero += 2
+numero -= 5
+numero *= 5
+numero /= 5
+
+print(numero)
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 % 3)
+print(2 ** 3)
