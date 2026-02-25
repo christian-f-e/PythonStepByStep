b269c698b668a5a06988385d2722daad6db3ae73
Christian.Fernandez <chrfereli@hotmail.com>
2026-02-25 15:20:39 -0600

    ConversionDeTipos.py

---
+++ Seccion2/ConversionDeTipos.py
@@ -0,0 +1,13 @@
+"""Conversion de tipos"""
+x = input("")
+#int() 
+#str()
+#float()
+#bool()
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
+#print(bool())
