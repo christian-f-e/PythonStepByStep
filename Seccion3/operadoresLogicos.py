ff3c0dd0f2dd9598c5b8530d0689b729cc7afdca
Christian.Fernandez <chrfereli@hotmail.com>
2026-03-02 17:17:26 -0600

    OperadoresLogicos.py

--- Seccion3/operadoresLogicos.py
+++ Seccion3/operadoresLogicos.py
@@ -1,1 +1,11 @@
+"""Operadores Logicos"""
+# and or not
 
+gas = True
+encendido = True
+edad = 18
+
+if gas and (encendido and edad > 17):
+    print('Puedes avanzar')
+else:
+    print('No puedes avanzar')
