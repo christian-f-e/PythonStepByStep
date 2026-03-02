f1120fc2a77e379c370926851546b09af8dc24f2
Christian.Fernandez <chrfereli@hotmail.com>
2026-03-02 16:09:46 -0600

    IfElse

---
+++ Seccion3/ifElse.py
@@ -0,0 +1,15 @@
+"""IF ELSE ELIF"""
+
+edad  = 43 
+
+if edad > 40 : 
+    print('Puede ver la pelicula con super descuento')
+elif edad > 30 : 
+    print('Puede ver la pelicula con descuento')
+elif edad > 17 : 
+    print('Puede ver la pelicula')
+else:
+    print('No puedes entrar')
+    
+print ('listo')
+
