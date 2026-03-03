56e64f437b3922572e4580c63b2489726ba4c9ca
Christian.Fernandez <chrfereli@hotmail.com>
2026-03-03 16:45:48 -0600

    CortoCircuito y CadenaDeComparadores

---
+++ Seccion3/cadenaDeComparadores.py
@@ -0,0 +1,9 @@
+"""Cadena de comparadores"""
+
+edad = 25
+
+if (edad >= 15 and edad <= 65):
+    print('Puede entrar a la picina')
+
+if (15 <= edad <= 65):
+    print('Puedes entrar a la alberca')
--- Seccion3/cortoCircuito.py
+++ Seccion3/cortoCircuito.py
@@ -1,3 +1,10 @@
 """Corto circuito"""
 
+gas = False
+encendido = True
+edad = 18
 
+if not gas and (encendido and edad > 17):
+    print('Puedes avanzar')
+else:
+    print('No puedes avanzar')
