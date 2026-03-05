6e65773439762f9674e3899ec48dbbc79fe61985
Christian.Fernandez <chrfereli@hotmail.com>
2026-03-04 18:12:53 -0600

    Parametros y Argumentos

---
+++ Seccion4/parametrosArgumentos.py
@@ -0,0 +1,13 @@
+"""Parametros y Argumentos"""
+
+nombre = 'Christian'
+apellido = 'Fernandez'
+
+
+def Hola(nombre, apellido):
+    print('Hola Mundo')
+    print(f'Bienvenido {nombre} {apellido}')
+
+
+Hola(nombre, apellido)
+Hola('Chachito','Feliz')
